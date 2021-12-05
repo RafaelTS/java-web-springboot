3dbbf7d4a3a64cb3e5a4efbb007a1918faa650c5
Rafael Teixeira <rafaeltorress@gmail.com>
2021-12-05 15:35:19 -0300

    uploading basic project

---
+++ src/main/java/br/sc/rafael/javawebspringboot/JavaWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.sc.rafael.javawebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sc/rafael/javawebspringboot/JavaWebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.rafael.javawebspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
